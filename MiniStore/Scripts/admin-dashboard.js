ed39aa9db80b6cf84bf2b00adad29f5729fbeb6c
Rob <robmars2008@gmail.com>
2018-08-12 01:29:07 -0500

    used fontawesome icons, applied js functions to icons, modified layouts.

--- MiniStore/Scripts/admin-dashboard.js
+++ MiniStore/Scripts/admin-dashboard.js
@@ -4,6 +4,8 @@
         window.location = $(this).data('href');
     });
 
+
+
 })
 
 
---
+++ MiniStore/Scripts/custom.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    //$('.fa-search').click(function () {
+    //    $('.search-bar').fadeToggle("slow", "linear");
+    //})
+
+    $(".search-bar").hide();
+})
+
+$(".fa-search").click(function () {
+    $(".search-bar").animate({
+        width: "toggle"
+    });
+});
